0631f787cb97bb84643ebaf891ace5d422e15a37
SlowV <quocviet.hn98@gmail.com>
2019-11-10 22:25:01 +0700

    init

---
+++ src/main/java/com/t1708e/taskunite/web/rest/HomeController.java
@@ -0,0 +1,19 @@
+package com.t1708e.taskunite.web.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/contact")
+    public String contact(){
+        return "contact";
+    }
+}
